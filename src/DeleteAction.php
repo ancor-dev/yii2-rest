53da8634a2ef73a3185c44b4349781be827756c5
Anton Korniychuk <ancor-root@bitbucket.org>
2016-01-11 18:03:39 +0200

    afterFind ready

--- src/DeleteAction.php
+++ src/DeleteAction.php
@@ -9,5 +9,39 @@ use yii\rest\DeleteAction as _DeleteAction;
  */
 class DeleteAction extends _DeleteAction
 {
+    /**
+     * @var callable a PHP callable that will be called after model successful found
+     * to additional checking operations. You can throw some exceptions from it
+     * if the this callable will return 'false', NotFoundHttpExceptions will be throw next
+     *
+     * ```php
+     * function ($model, $action) {
+     *     if ($model->status == $model::STATUS_DELETED) return false; // Not found
+     *
+     *     if ($model->checking == 2) { // example
+     *
+     *         throw new SomeException(...);
+     *     }
+     * }
+     * ```
+     */
+    public $afterFind;
 
+    public function afterFind()
+    {
+        if ($this->findModel !== null) {
+            return call_user_func($this->findModel, $id, $this);
+        }
+    }
+
+    public function findModel($id)
+    {
+        $model = parent::findModel($id);
+
+        if ($this->afterFind($model, $this) === false) {
+            throw new NotFoundHttpException("Object not found: $id");
+        }
+
+        return $model;
+    }
 }
